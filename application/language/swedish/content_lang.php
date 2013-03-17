f593fe32fb3bcdcdab7bbef3c5da1c07f32fb0eb
Freddie Frantzen <freddie@freddiefrantzen.com>
2013-03-17 03:42:48 +0000

    made some changes

--- application/language/swedish/content_lang.php
+++ application/language/swedish/content_lang.php
@@ -1,4 +1,3 @@
-
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
@@ -31,22 +30,22 @@ $lang['home_keywords'] =   'swedish to english,
 
 $lang['services_desc'] = 'Breakdown of available translation services.';
 
-$lang['services_keywords'] = 'matilda, eriksson, translation, services, uk, freelance, english, sweedish';
+$lang['services_keywords'] = 'matilda, eriksson, translation, services, uk, freelance, english, swedish';
 
-$lang['clients_desc'] = 'Details of previous employment, past clients and testermonials';
+$lang['clients_desc'] = 'Details of previous employment, past clients and testimonials';
 
-$lang['clients_keywords'] = 'matilda, eriksson, translation, services, uk, freelance, english, sweedish';
+$lang['clients_keywords'] = 'matilda, eriksson, translation, services, uk, freelance, english, swedish';
 
 $lang['contact_desc'] = 'Get in touch for a free quote. Send me a message or friend me on LinkedIn';
 
-$lang['contact_keywords'] = 'matilda, eriksson, translation, services, uk, freelance, english, sweedish';
+$lang['contact_keywords'] = 'matilda, eriksson, translation, services, uk, freelance, english, swedish';
 
 // Menu, section and page titles
 
 $lang['home_page'] = 'Hem';
 $lang['services_page'] = 'Tjänster';
-$lang['clients_page'] = 'Klienter';
-$lang['contact_page'] = 'Kontact';
+$lang['clients_page'] = 'Min bakgrund och klienter';
+$lang['contact_page'] = 'Kontakta mig';
 
 // Misc
 
@@ -54,8 +53,8 @@ $lang['site_by'] = 'Design and Code';
 
 // Quotes
 
-$lang['quote1_body'] = '&quot;Det var en fröjd att arbeta med dig i detta projektet. Du levererade som du utlovat, tog ansvar, ifrågasatte det som var oklart, skötte kontakter, var anträffbar och framför allt hanterade du det engelska språket enkelt och elegant. Självklart rekommenderar jag dig.&quot;';
-$lang['quote1_source'] = 'Ulla Kastrup Sjunnesson, projektledare för det internationella konstprojektet Sharing Waters, Istanbul';
+$lang['quote1_body'] = '&quot;Det var en fröjd att arbeta med dig i det här projektet. Du levererade som du utlovat, tog ansvar, ifrågasatte det som var oklart, skötte kontakter, var anträffbar och framför allt hanterade du det engelska språket enkelt och elegant. Självklart rekommenderar jag dig.&quot;';
+$lang['quote1_source'] = 'Ulla Kastrup Sjunnesson, projektledare för det internationella konstprojektet Sharing Waters, Stockholm/Istanbul';
 
 $lang['quote2_body'] = 'Matilda visade goda kunskaper i det svenska och engelska språket och har förstått och på ett mycket bra sätt tolkat innehållet av budskapet i presentationen. Uppgifterna har hon utfört mycket bra, hon har snabbt, enkelt och med en god kvalité gjort översättningarna. Jag vill därför varmt rekommendera henne till liknande uppgifter.';
 $lang['quote2_source'] = 'Margareta Fintling, socialkonsulent, Arbetsförmedlingen';
@@ -68,27 +67,27 @@ $lang['quote3_source'] = 'Some Person, Some Company';
 
 // Home page
 
-$lang['home_intro'] =      "Välkommen till Matilda Eriksson Translation Services. Hit kan du vända dig om du behöver hjälp med textredigering, språkgranskning och översättning från svenska till engelska samt från engelska till svenska. ";
+$lang['home_intro'] =      "Välkommen till Matilda Eriksson Translation Services. Hit kan du vända dig om du behöver hjälp med textredigering, språkgranskning och översättning från svenska till engelska och från engelska till svenska. ";
 
 // Services page
 
 $lang['services_byline'] = 'Kvalitetsöversättning till rimliga priser.';
 $lang['services_intro'] = "<p>
-                            Jag översätter från svenska till engelska och från engelska till svenska men erbjuder även språkgranskningstjänster till uppdragsgivare som föredrar att själv skriva texten på målspråket. Med min hjälp kan du vara säker på att grammatiken håller, samt undvika att stavfel eller ’svengelska’ fraser slinker med. Jag har översatt en mängd olika material, bland annat kataloger, offerter och tidningsartiklar. Mina klienter spänner över ett brett spektrum. Många av mina uppdrag kommer från tidningar, intresseorganisationer, företag och privatpersoner. Jag vurmar för litteratur och kultur i allmänhet och har översatt allt ifrån konstkataloger, dikter och skönlitteratur, men jag har även erfarenhet av mer tekniska texter, specifikt inom finans och inom kamera- och mobilteknologi.
+                            Jag översätter från svenska till engelska och från engelska till svenska men erbjuder även språkgranskningstjänster till uppdragsgivare som föredrar att själv skriva texten på målspråket. Med min hjälp kan du vara säker på att grammatiken håller, samt undvika att stavfel eller ’svengelska’ fraser slinker med. Jag har översatt en mängd olika material, bland annat kataloger, offerter och tidningsartiklar. Mina klienter spänner över ett brett spektrum. Många av mina uppdrag kommer från tidningar, myndigheter, intresseorganisationer, företag och privatpersoner. Jag vurmar för litteratur och kultur i allmänhet och har översatt allt ifrån konstkataloger, dikter och skönlitteratur, men jag har även erfarenhet av mer tekniska texter, specifikt inom finans och inom kamera- och mobilteknologi.
                             </p>";
 
 
-$lang['service1_head'] = 'Swedish-English and English-Swedish Translation';
-$lang['service1_body'] = 'With several years experience, specialising in media translations.';
+$lang['service1_head'] = 'Översättning från svenska till engelska och engelska till svenska';
+$lang['service1_body'] = 'Professionell högkvalitativ översättning';
 
-$lang['service2_head'] = 'Copy-editing and proofreading';
-$lang['service2_body'] = 'Swedish and English copy-editing and proofreading services also available.';
+$lang['service2_head'] = 'Språkgranskning och korrekturläsning';
+$lang['service2_body'] = 'Mångårig erfarenhet av språkgranskning, korrekturläsning och redigering';
 
-$lang['service3_head'] = 'Media Analysis';
-$lang['service3_body'] = 'Experience analysing online media for internationally renowned brands.';
+$lang['service3_head'] = 'Kvalitetsgranskning och lokalisation';
+$lang['service3_body'] = 'Har du redan anlitat en översättare men är osäker på om kvaliteten i texten håller? Min bakgrund inom språkgranskning garanterar en professionell utvärdering av översättning och lokalisation';
 
-$lang['service4_head'] = 'Fast and efficient';
-$lang['service4_body'] = 'I will meet your deadlines and deliver first-class copy.';
+$lang['service4_head'] = 'Snabb och effektiv';
+$lang['service4_body'] = 'Mina klienter kan alltid lita på ett väl utfört arbete som levereras på eller innan utsatt tid.';
 
 // Clients page
 
@@ -106,36 +105,28 @@ $lang['testimonials_head'] = 'Testimonials';
 
 // Contact page
 
-$lang['contact_getintouch'] = 'Get In Touch';
-$lang['contact_contactingme'] = 'Contacting Me';
+$lang['contact_getintouch'] = 'Kontakta mig';
+$lang['contact_contactingme'] = 'Kontakta mig';
 
 $lang['contact_intro'] = "  <p> 
-                            Drop me a line if you'd like to discuss a project or to to obtain a free quote. I'll get right back to you.
+                            Hör gärna av dig om du vill diskutera ett projekt. Snabbt svar och gratis offert garanteras. 
                             </p> 
  
                             <p> 
-                            I'm also on".' <a href="http://uk.linkedin.com/in/matildaueriksson">LinkedIn</a>'." - feel free to add me.
-                            If you like, you can follow me on".' <a href="http://twitter.com/#!/MatildasCorner">Twitter</a>'." too.
-                            If you'd prefer to talk via old-fashioned means you can call me on my UK mobile, which is +44 (0) 7783794944. 
+                            Det går bra att lägga till mig på".' <a href="http://uk.linkedin.com/in/matildaueriksson">LinkedIn</a>'." eller varför inte följa mig på".' <a href="http://twitter.com/#!/MatildasCorner">Twitter</a>'.".
+                            Jag nås även på telefon (+44 (0) 7783794944) samt Skype: matilda.eriksson81
                             </p> 
- 
-                            <p> 
-                            Originally from Sweden, I now live and work from Brighton, England, and travel frequently to and from London and Stockholm.
-                            </p> 
-
-                            <p> 
-                            I look forward to hearing from you!
-                            </p> 
+                         
 
                             <p> 
                             / Matilda
                             </p> ";
 
 
-$lang['contact_name'] = 'Your Name';
-$lang['contact_email'] = 'Your Email';
-$lang['contact_message'] = 'Your Message';
-$lang['contact_submit'] = 'Send Message';
+$lang['contact_name'] = 'Ditt Namn';
+$lang['contact_email'] = 'Din Email';
+$lang['contact_message'] = 'Ditt Meddelande';
+$lang['contact_submit'] = 'Skicka Meddelande';
 
 // For Form Validation
 
@@ -143,7 +134,7 @@ $lang['name'] = 'Namn';
 $lang['email'] = 'E-post';
 $lang['message'] = 'Meddelande';
 
-$lang['email_send_error'] = "I'm afraid there was a problem sending your email Please try again later or contact me by telephone";
-$lang['email_send_success'] = "Thank you. Your message has been received";
+$lang['email_send_error'] = "Oj, nu blev det något fel. Försök om en stund igen är du snäll eller ring mig istället.";
+$lang['email_send_success'] = "Tack! Ditt meddelande är mottaget";
 
 // Form field Error Messages - See languages/[language]/form_validation_lang.php

87231f1c5aca4ace8ab1e23cbb9e1d94d3bd64a6
freddiefrantzen <freddie@freddiefrantzen.com>
2020-05-04 00:20:26 +0100

    Converted to static site

--- application/views/partials/nav.php
+++
@@ -1,9 +0,0 @@
-<div id="logo">
-</div>
-
-<ul id="top-nav">
-    <li><a id="home-link" href="<?php echo base_url().$this->config->item('language_abbr');?>/home"><?php echo $this->lang->line('home_page'); ?></a></li>
-    <li><a id="services-link" href="<?php echo base_url().$this->config->item('language_abbr')?>/services"><?php echo $this->lang->line('services_page'); ?></a></li>
-    <li><a id="clients-link" href="<?php echo base_url().$this->config->item('language_abbr');?>/work"><?php echo $this->lang->line('clients_page'); ?></a></li>
-    <li><a id="contact-link" href="<?php echo base_url().$this->config->item('language_abbr');?>/contact"><?php echo $this->lang->line('contact_page'); ?></a></li>
-</ul>
